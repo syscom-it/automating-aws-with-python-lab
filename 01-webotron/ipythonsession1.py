695abff262d0a9b947a2790234b826ddc30c869d
Linton <linton@Lintons-MacBook-Pro.local>
2020-05-18 13:00:55 +0100

    Setup directory project for webotron

---
+++ 01-webotron/ipythonsession1.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='Python')
+s3 = session.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket)
+
+for bucket in s3.buckets.all():
+    print(bucket)
+
+new_bucket = s3.create_bucket(Bucket='pythonlab2')
+for bucket in s3.buckets.all():
+    print(bucket)
+
+get_ipython().run_line_magic('history', '')
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Automating AWS With Python
 
 Repository for Acloudguru course *Automating AWS With Python*
+## 01-webotron
+
+Webotron is a script that will sync a local directory to a s3 bucket, and optionally configure Route53 and cloudfront as well.  
